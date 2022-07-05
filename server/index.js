be5bb4dffe08f359813267532eb9395997ad5692
unknown <guiratwalid98@gmail.com>
2022-07-05 14:59:25 +0100

    Database connection

---
+++ server/index.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+const MONGODB_URL = process.env.MONGODB_URL;
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(express.json({
+    limit: "30mb",
+    extended: true,
+}));
+app.use(express.urlencoded({
+    limit: "30mb",
+    extended: true,
+}));
+app.use(cors());
+
+mongoose.connect(MONGODB_URL)
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        });
+    })
+    .catch(err => {
+        console.log(`Server did not connect.\n${err} `);
+    });
