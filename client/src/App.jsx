addb8e18bce8583deb898afff3e39f6d805f9db2
unknown <guiratwalid98@gmail.com>
2022-07-07 13:57:57 +0100

    Login Page

---
+++ client/src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css';
+import { ToastContainer, toast } from 'react-toastify';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import "react-toastify/dist/ReactToastify.css";
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <ToastContainer />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+
+export default App;
